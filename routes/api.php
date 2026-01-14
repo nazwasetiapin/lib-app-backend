3a773445b5f5bd627890bb6d90aa91bd693a0ac9
nazwastpn <nazwasetiapin@gmail.com>
2026-01-14 11:26:46 +0700

    setup api routing, cors, and sanctum authentication

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/ping', function () {
+    return response()->json([
+        'status' => 'ok',
+        'message' => 'API is running'
+    ]);
+});
